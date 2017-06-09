58e08baf47d982d374d0a739491aca5942b2b05f
Lloyd <Driven.lloyd@gmail.com>
2017-06-09 10:51:09 +0100

    added animation starter content
    Added animation starter content to help with setting up the characters
    animation and the controller.

--- Ava/Intermediate/Build/Win64/UE4Editor/Development/Ava/Ava.h
+++ Ava/Intermediate/Build/Win64/UE4Editor/Development/Ava/Ava.h
@@ -1,5 +1,5 @@
 #ifndef __AUTO_AVA_H__
 #define __AUTO_AVA_H__
-//Last Write: 05/06/2017 09:06:15 +00:00
-#include "W:\University Work\Masters\MastersProject\Ava\Source\Ava\Ava.h"
+//Last Write: 09-Jun-17 8:52:24 AM +00:00
+#include "G:\University Work\Masters\MastersProject\MastersProject\Ava\Source\Ava\Ava.h"
 #endif//__AUTO_AVA_H__
