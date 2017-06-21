c8e65023884d5c2b8893bd3f4a31d3d51fda2ee6
Driven-Mad <Driven.lloyd@gmail.com>
2017-06-21 08:29:58 +0100

    Quick clean up.
    Quick clean up and added external map

--- Ava/Intermediate/Build/Win64/UE4Editor/Development/Ava/Ava.h
+++ Ava/Intermediate/Build/Win64/UE4Editor/Development/Ava/Ava.h
@@ -1,10 +1,5 @@
 #ifndef __AUTO_AVA_H__
 #define __AUTO_AVA_H__
-<<<<<<< HEAD
-//Last Write: 20/06/2017 12:58:04 +00:00
+//Last Write: 21/06/2017 06:50:44 +00:00
 #include "C:\Users\i7230020.STUDENT.000\Documents\GitHub\MastersProject\Ava\Source\Ava\Ava.h"
-=======
-//Last Write: 19/06/2017 09:44:13 +00:00
-#include "D:\Reka\MASTER PROJECT\MastersProject\MastersProject\Ava\Source\Ava\Ava.h"
->>>>>>> 18adbce33b5af7194324dfbbd642ac7bf48c768d
 #endif//__AUTO_AVA_H__
